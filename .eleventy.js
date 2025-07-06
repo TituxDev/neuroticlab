8bbfa62d27107f757e85893f84b413a365305ca9
TituxDev <ocrstmy@gmail.com>
2025-07-06 11:00:35 -0600

    Moving site content from _site to docs

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",     // Carpeta fuente
-      output: "_site"   // Carpeta de salida
+      output: "docs"   // Carpeta de salida
     }
   };
 };
