e22245dbe928bec5f05a349f6364a28e3ac57bc6
TituxDev <ocrstmy@gmail.com>
2026-04-16 11:50:12 -0600

    sitemap added

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml");
   return {
     dir: {
       input: "src",     // Carpeta fuente
